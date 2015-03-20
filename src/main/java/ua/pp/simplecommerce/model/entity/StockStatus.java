c9d8dba4cf2749f636d8a42ad0cd92b07b911b75
standpoint <v.kamenskyi@gmail.com>
2015-03-20 17:19:46 +0200

    Entities. Creation
    enum StockStatus has been isolated from the class 'Product' into standalone file

---
+++ src/main/java/ua/pp/simplecommerce/model/entity/StockStatus.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2015. SimpleCommerce.pp.ua
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package ua.pp.simplecommerce.model.entity;
+
+/**
+ * Status of the products presence at the store warehouse
+ *
+ * Created by Vladimir Kamenskiy on 18.03.2015.
+ */
+public enum StockStatus {
+    IN_STOCK,
+    OUT_OF_STOCK,
+    EXPECTED,
+    LIMITED_STOCK
+}
