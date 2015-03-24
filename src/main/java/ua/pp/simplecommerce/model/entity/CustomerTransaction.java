4f9f4cf6d9a24397c91cbcd678755e949da061d1
standpoint <v.kamenskyi@gmail.com>
2015-03-24 11:30:08 +0200

    Entities. Creation
    CustomerTransaction entity has been added

---
+++ src/main/java/ua/pp/simplecommerce/model/entity/CustomerTransaction.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2015. SimpleCommerce.pp.ua
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package ua.pp.simplecommerce.model.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Calendar;
+
+/**
+ * The entity 'CustomerTransaction' has info about customer' transactions - such as summary info about customer' order
+ * (Order entity, summary information, total order amount, date and time when the order has been added)
+ *
+ * Created by Vladimir Kamenskiy on 23.03.2015.
+ */
+
+@Entity
+@Table(name = "customer_transaction")
+public class CustomerTransaction {
+
+    private Long customerTransactionId;
+    private Order order;
+    private String summary;
+    private BigDecimal amount;
+    private Calendar dateAdded;
+
+    public CustomerTransaction() {
+    }
+
+    public CustomerTransaction(Long customerTransactionId, Order order, String summary, BigDecimal amount,
+                               Calendar dateAdded) {
+        this.customerTransactionId = customerTransactionId;
+        this.order = order;
+        this.summary = summary;
+        this.amount = amount;
+        this.dateAdded = dateAdded;
+    }
+
+    @Id @GeneratedValue
+    @Column(name = "customer_transaction_id")
+    public Long getCustomerTransactionId() {
+        return customerTransactionId;
+    }
+
+    public void setCustomerTransactionId(Long customerTransactionId) {
+        this.customerTransactionId = customerTransactionId;
+    }
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_transaction_order_fk", nullable = false)
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Column(nullable = false, length = 2000)
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    @Column(nullable = false, scale = 2)
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    @Column(name = "date_added")
+    @Temporal(value = TemporalType.TIMESTAMP)
+    public Calendar getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(Calendar dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+}
